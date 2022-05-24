a959b3431bcc13a551c66c91e5fa1eef1dd39fcd
lukecylu <lukecylu@tencent.com>
2022-05-24 16:33:40 +0800

    lfu

--- data-structure/460.lfu-cache.ts
+++ data-structure/460.lfu-cache.ts
@@ -6,16 +6,96 @@
 
 // @lc code=start
 class LFUCache {
+    capacity : number =0 ;
+    minFreq: number = 1;
+    size: number = 0;
+    keyToVal:  Map<number, number>;
+    keyToFrq : Map<number, number>;
+    freqToKey: Map<number, Map<number,boolean> >;
     constructor(capacity: number) {
-
+      this.capacity = capacity;
+      this.keyToVal = new Map();
+      this.keyToFrq = new Map();
+      this.freqToKey =  new Map();
     }
 
     get(key: number): number {
+      /// exist, return val and update freq;
+      if(this.keyToVal.has(key)){
+        this.increaseFreq(key);
+        return this.keyToVal.get(key);
+      }
+      /// not exits, return -1
+      return -1;
 
     }
 
     put(key: number, value: number): void {
+      /// avoid 0 capacity condition
+      if(this.capacity == 0){
+        return;
+      }
+      /// exist, update val and update freq;
+      if(this.keyToVal.has(key)){
+        this.increaseFreq(key);
+        this.keyToVal.set(key,value);
+        return;
+      }
+      /// not exist, then insert value and check if size is larger than cap;
+      this.size++;
 
+      /// first delete 
+      if(this.size > this.capacity){
+        this.deleteLFU();
+      }
+      /// then add new
+      this.keyToVal.set(key,value);
+      this.increaseFreq(key);
+      this.minFreq = 1;
+
+    }
+
+
+    deleteLFU(): void{
+      if(!this.freqToKey.has(this.minFreq)){
+        return;
+      }
+      this.size--;
+      let minFreqMap = this.freqToKey.get(this.minFreq);
+      let key = null;
+      for (let [k, _] of minFreqMap) {
+        minFreqMap.delete(k);
+        key = k;
+        /// only delete the first key
+        break;
+      }
+      this.keyToFrq.delete(key);
+      this.keyToVal.delete(key);
+
+    }
+
+    increaseFreq(key: number){
+      let prevFreq = this.keyToFrq.has(key) ? this.keyToFrq.get(key)  : 0;
+      let newFreq = prevFreq + 1;
+      this.keyToFrq.set(key,  prevFreq + 1);
+
+      /// delete key from prevFreq map in freqToKey map
+      if(prevFreq > 0){
+        const freqMap = this.freqToKey.get(prevFreq);
+        freqMap.delete(key);
+      }
+
+      // / add key to newFreq map in freqToKey map
+      if(!this.freqToKey.has(newFreq)){
+        this.freqToKey.set(newFreq, new Map<number, boolean>());
+      }
+      this.freqToKey.get(newFreq).set(key, true);
+
+      // only update freq here
+      if(prevFreq ==  this.minFreq && this.freqToKey.get(prevFreq).size == 0){
+        this.minFreq = newFreq;
+      }
+      
     }
 }
 
